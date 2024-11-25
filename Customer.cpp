8ab69f36d14ddc177bf5290e6df08ae64319e1f8
TheGasz <bagasadjiepamungkas2006@mail.ugm.ac.id>
2024-11-25 21:43:03 +0700

    modified:   Customer.cpp 	modified:   .vscode/settings.json 	modified:   Customer.h

--- Customer.cpp
+++ Customer.cpp
@@ -4,11 +4,33 @@
 // #include "Car.h"
 
 using namespace std;
+ // Alokasi dinamis untuk 10 Customer
 
-Customer::Customer(int j): customer(j) {}
+
+
+
+
+Customer::Customer(): customer() {}
 
 void Customer::customerRegister(const string &username , const string &password) {
+    string username1 ;
+    string password1 ;    
     
-    cout << "halo";
+    cout << "Masukkan UserName njenengan : " ;
+    cin >> username1 ;
+
     
+};
+
+void Customer  :: set_name(string username1){
+    Customer customer[1];
+    
+    this->name = username1; // Mengubah atribut 'name' dari objek saat ini
+
+
+};
+
+
+void addCustomer(){
+    Customer* customers = new Customer[];
 }
