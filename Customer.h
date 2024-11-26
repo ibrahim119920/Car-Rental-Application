574a6fc15f67aa623671510e4ade620f067bb662
TheGasz <bagasadjiepamungkas2006@mail.ugm.ac.id>
2024-11-26 19:19:45 +0700

    modified:   Customer.h 	modified:   .vscode/settings.json 	modified:   Car.cpp 	deleted:    Customer.exe

--- Customer.h
+++ Customer.h
@@ -18,14 +18,14 @@ public:
     Customer(string name1, string email1, string customerPassword1, string customerID1);
     Customer();
     static int checkDatabase(string name, string password); // Update in the header file
-    string get_name();
-    string get_email();
-    string get_customerPassword();
-    string get_customerID();
+    string get_name(string name);
+    string get_email(string email);
+    string get_customerPassword(string password);
+    string get_customerID(string customerID);
     static void customerRegister();
     // static void displayCustomers(); // Tambahkan metode untuk menampilkan daftar pelanggan
     static void customerLogin();
-    void viewAvailableCars();
+    static  void viewAvailableCars();
     int bookCar();
     int extendRent();
     void returnCar();
