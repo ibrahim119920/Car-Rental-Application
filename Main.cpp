2e245a87737c59da4565614f26b72b3fb74d2edd
ibrrhmmm <ahmadmaulanaibrahim@mail.ugm.ac.id>
2024-11-26 19:02:20 +0700

    deleted:    Admin.cpp 	deleted:    Admin.h 	modified:   Booking.cpp 	modified:   Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <string>
-#include "Admin.h"
 
 using namespace std;
 
 int main() {
-    string username = "admin";
-    string password = "admin";
-    Admin tes(1);
-    tes.adminLogin(username, password);
 
     return 0;
 }
