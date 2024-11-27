6bd6b9c2b678436e486817943d1623c79563a51b
ibrrhmmm <ahmadmaulanaibrahim@mail.ugm.ac.id>
2024-11-27 11:57:58 +0700

    deleted:    Controller.cpp 	deleted:    Controller.h 	modified:   Customer.h

--- Customer.h
+++ Customer.h
@@ -15,6 +15,7 @@ private:
     string customerPassword;
 
 public:
+    
     Customer(string name1, string email1, string customerPassword1, string customerID1);
     Customer();
     static int checkDatabase(string name, string password); // Update in the header file
@@ -23,7 +24,6 @@ public:
     string get_customerPassword(string password);
     string get_customerID(string customerID);
     static void customerRegister();
-    // static void displayCustomers(); // Tambahkan metode untuk menampilkan daftar pelanggan
     static void customerLogin();
     static void customerMenu();
     static void viewAvailableCars();
